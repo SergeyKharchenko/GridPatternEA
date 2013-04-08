4882171d3997d78f168a88b26d8648f59f750373
Sergey Kharchenko <hsvlis4@gmail.com>
2013-04-08 21:57:04 +0300

    add IPatternParser

---
+++ GridPattern/GridPatternLibrary/Helpers/Abstract/IPatternParser.cs
@@ -0,0 +1,10 @@
+﻿namespace GridPatternLibrary.Helpers.Abstract
+{
+    public interface IPatternParser
+    {
+        string RemoveSpaces(string data);
+        string RemoveFirstRow(string data);
+        string[] GetRows(string data);
+        string[] RemoveFirstColumn(string[] data);
+    }
+}
